ff5dc875482f4b1c875acb658ec2ab6611bfa207
Mahdi Hmissi <midouhmissi1@gmail.com>
2021-02-28 18:18:54 -0500

    Final class file without test

--- Classe_Final.js
+++ Classe_Final.js
@@ -1,114 +1,145 @@
-class Aliment {
-    constructor (nom, poids) {
-        this.nom = nom;
-        this.poids = poids;
+class Aliment{
+    constructor(nom, poids){
+        this.nom = nom; 
+        this.poids = poids; 
     }
 
+    informationAliments(){
+        return this.nom + ", " + this.poids;  
+    }
+    
 }
 
-class Croute extends Aliment {
+class Croute extends Aliment{
     constructor(nom, poids, type, prix, taille){
+    super(nom, poids);
+    this.type = type ;  
+    this.prix = prix;
+    this.taille = taille;
+    } 
+
+    prixCroute(){
+        return (this.taille * this.prix);  
+    }
+
+
+}
+
+class Fromage extends Aliment{ 
+  constructor(nom, poids, prixFromage){
+    super(nom, poids);
+    this.prixFromage = prixFromage;  
+    }
+    prixFromage(){ 
+        return (this.poids * this.prixFromage );
+    }
+} 
+class Garniture extends Aliment{
+    constructor(nom, poids, prixGarniture){
         super(nom, poids);
-        this.type = type;
-        this.prix = prix;
-        this.taille = taille;
-    }
-
-    //GV: les noms des méthodes doit commencer par minuscule
-    TailleCroute(TailleCrouteMenu){ 
-        this.TailleCroute
-        switch(TailleCrouteMenu){
-            case 1:
-                TailleCroute = "Petite";
-                break;
-            case 2: 
-                TailleCroute = "Moyenne";
-                break;
-            case 3:
-                TailleCroute = "Grande";
-                break;
-            default:
-                console.log = "Mauvaise valeur";
-                break;
+        this.prixGarniture = prixGarniture;  
         }
-    }
-    PrixCroute(PrixCroute){ //GV: L'attribut prix de la croute a un valeur fixe.
-        switch(TailleCrouteMenu){
-            case 1:
-                PrixCroute = 0;
-                break;
-            case 2: 
-                PrixCroute = 0;
-                break;
-            case 3:
-                PrixCroute = 0;
-                break;
-            default:
-                console.log = "Mauvaise valeur";
-                break;
+        informationGarniture(){
+            return this.nom + ", " + this.poids + ", " + this.garniture + ", " + this.prixGarniture ;
         }
-    }
-}
-
-class Fromage extends Aliment {
-    constructor(nom, poids, prix) {
+        prixGarniture(){
+            return (this.poids * this.prixGarniture); 
+            
+        }
+} 
+class Epices extends Epices {
         super(nom, poids);
-        this.prix = prix;
-    }
-
-    //GV: Il manque les méthodes
-}
-
-class Garniture extends Aliment {
-    constructor(nom, poids, prix) {
-        super(nom, poids)
-        this.prix = prix
-    }
-
-    //GV: Il manquel es méthodes
-}
-
-class Epices extends Aliment {
-    constructor(nom, poids, epices) { //GV: X Pas besoin de constructor
-        super(nom, poids)
-        this.epices = epices;
-    }
 }
 
 class Pizza {
-    constructor(numeroDePizza, taille, croute, tempsDeCuisson) {
-        this.numeroDePizza = numeroDePizza;
-        this.taille = taille;
-        this.croute = croute;
-        this.tempsDeCuisson = tempsDeCuisson;
-        //GV: fromages
-        //GV: garnitures
-        //GV: prixComplet
-        //GV: tempsCuisson
+    constructor(numeroPizza, taille, croute, fromage, garniture,prixComplet, tempsCuisson ){
+    this.prixComplet = prixComplet ; 
+    this.tempsCuisson = tempsCuisson ; 
+    this.numeroPizza = numeroPizza; 
+    this.taille = taille; 
+    this.croute = croute ; 
+    this.fromage = fromage ; 
+    this.garniture = garniture; 
+    
+    }
+    informationPizza(){ 
+        return this.numeroPizza + ", " + this.taille + ", " + this.croute + ", " + this.fromage + ", " + this.garniture + ", " + this.prixComplet + ", " + this.tempsCuisson 
     }
 
-    //GV: il manque les méthodes
+    infoTempsCuisson(){
+            return this.TempsCuisson;
+    }
+    modifierTempsCuisson(nouveauTempsCuisson) { 
+            this.tempsCuisson = nouveauTempsCuisson;
+    }
+    
+    infoTailleCroute(){
+        return this.croute ;  
+    }
+    modifierCroute(){
+        this.croute = nouvelleCroute ; 
+    }
+
+    infoTaille(){
+        return this.taille; 
+    }  
+    modifierTaille(nouvelleTaille){
+        this.taille = nouvelleTaille ; 
+    }
+    
+    infosGarniture(){
+        return this.garniture ; 
+    }
+        
+    ajouterGarniture(garniture){
+
+        this.garnitures(this.garnitures.length) = garniture;
+    
+    }
+    
+    infosFromage(){
+        return this.fromage ; 
+
+    }
+
+    ajouterFromage(){
+        this.fromages(this.fromages.length) = fromage; 
+    }
+
+    calculPrixComplet(prixComplet){
+        (( this.garniture + this.fromage + this.croute ) * this.taille) = prixComplet;    
+    }
+
+
+
+
 }
 
-class Commande{
-    constructor(numeroDeCommande, dateDeLaCommande, heureDeLaCommande, pizzas, montanttotal){
+class Commande extends Pizza{ 
+    constructor(numCommande, dateCommande, heureCommande){
+    super(this.numeroPizza, prixComplet)
+    this.numCommande = numCommande ; 
+    this.dateCommande = dateCommande ; 
+    this.heureCommande = heureCommande ; 
 
+    
     }
-
-    //GV: Incomplet
+    informationCommande(){
+        return this.numCommande + ", " + this.dateCommande + ", " + this.heureCommande + ", " + this.nom ; 
+    }
 }
 
 class Client {
-    constructor(nom, prenom, numeroDeTelephone, adresseCourriel, numeroDeCommande){
-        this.nom = nom;
-        this.prenom=prenom;
-        this.numeroDeTelephone = numeroDeTelephone;
-        this.adresseCourriel = adresseCourriel;
-        this.numeroDeCommande = numeroDeCommande; //GV: commande (pas le numéro de commande, c'est la commande au complet)
+    constructor(nom, prenom, numTel, adresseCourriel){
+        super(commande)
+        this.nom= nom ; 
+        this.prenom = prenom; 
+        this.numTel = numTel; 
+        this.adresseCourriel =adresseCourriel;  
+        
+    }
+    informationClient(){
+        return this.nom + ", " + prenom + ", " + numTel + ", " + this.adresseCourriel
     }
 }
-
-
-
-
 
